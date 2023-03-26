4758dccade023d8e6b409fd577f812883535242a
Andrei Silviu Dragnea <andreisilviudragnea@gmail.com>
2023-03-26 22:17:51 +0300

    Echo input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    println!("{buffer}");
+    Ok(())
 }
